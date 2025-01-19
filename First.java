f1cde1d7a780a4d27218290466e010468528f63b
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-01-18 22:45:57 -0500

    Learning Purpose!

--- First.java
+++ First.java
@@ -1,8 +1,19 @@
 public class First{
     public static void main(String[] args){
-        int num = 55;
-        System.out.println(num++);
-        System.out.println(num);
-        System.out.println("Hello DSA!");
+        // int num = 55;
+        // System.out.println(num++);
+        // System.out.println(num);
+        // System.out.println("Hello DSA!");
+
+        // Data types:
+        // Two Types: Primitive Data Type: Byte, short, char, int, float, double, long, boolean
+        // Non Primitive Data Type: Classes, Interfaces, Arrays
+
+        // Boolean check_value = true;
+        // System.out.println(check_value);
+
+        byte a = 10;
+        System.out.println(a);
+    
     }
 }

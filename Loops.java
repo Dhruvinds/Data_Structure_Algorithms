1c207846d64587b0b94a1f6e9808a75451b16e9d
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-01-30 22:29:41 -0500

    Loops While and For Loop!

---
+++ Loops.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Loops {
+    public static void main(String args[]) {
+        // int number = 1;
+        // while(number <=10){
+        // System.out.print(number + " ");
+        // number++;
+
+        // Scanner sc = new Scanner(System.in);
+        // int number = sc.nextInt();
+        // int range = 1;
+
+        // while (range <= number) {
+        // System.out.print(range + " ");
+        // range++;
+        // int sum = 0;
+
+        // while (range <= number) {
+        // sum = sum + range;
+        // range++;
+        // }
+        // System.out.println(sum);
+
+        // For Loops!
+
+        // ****
+        // ****
+        // ****
+        // ****
+        // int line = 4;
+        // for(int i = 1; i<=line; i++){
+        // System.out.println("****");
+        // }
+
+        // Reverse Number -> 12345 -> 54321
+        // int number = 12345;
+        // int reverse = 0;
+        // while(number>0){
+        //     int remainder = number % 10;
+            // reverse = reverse * 10 + remainder;
+        //     System.out.print(remainder);
+        //     number = number/10;
+        // }
+    }
+}

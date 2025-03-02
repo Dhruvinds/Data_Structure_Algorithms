51f058a7914423e17955e4e9ace9e5934f467222
Dhruvin Sorathiya <dsorathi@uoguelph.ca>
2025-03-02 13:25:00 -0500

    done!

--- Function.java
+++ Function.java
@@ -1,12 +1,20 @@
 public class Function{
 
-    public static float avgOfNum(int a, int b, int c){
-        int sum = a + b + c;
-        int avg = sum/3;
-        return avg;
+    // public static float avgOfNum(int a, int b, int c){
+    //     int sum = a + b + c;
+    //     int avg = sum/3;
+    //     return avg;
+    // }
+
+    public static boolean isEven(int n){
+        if(n % 2 == 0){
+            return true;
+        }
+        return false;
     }
 
     public static void main(String args[]){
-        System.out.println(avgOfNum(10, 10, 10));
+        // System.out.println(avgOfNum(10, 10, 10));
+        System.out.println(isEven(499));
     }
 }

383c0019163e05712f1af1b197700fe89aeb4592
Dhruvin Sorathiya <dsorathi@uoguelph.ca>
2025-03-02 14:10:41 -0500

    check Palidrome

--- Function.java
+++ Function.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Function{
 
     // public static float avgOfNum(int a, int b, int c){
@@ -6,15 +7,37 @@ public class Function{
     //     return avg;
     // }
 
-    public static boolean isEven(int n){
-        if(n % 2 == 0){
-            return true;
+    // public static boolean isEven(int n){
+    //     if(n % 2 == 0){
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    public static boolean isPalindrome(int n){
+        int myNum = n;
+        int reverse = 0;
+        while(n>0){
+            int remainder = n % 10;
+            reverse = reverse * 10 + remainder;
+            n = n / 10;
+            // System.out.println(reverse);
         }
-        return false;
+        return myNum == reverse;
     }
 
     public static void main(String args[]){
         // System.out.println(avgOfNum(10, 10, 10));
-        System.out.println(isEven(499));
+        // System.out.println(isEven(499));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        scanner.close();
+        
+        if (isPalindrome(num)) {
+            System.out.println(num + " is a palindrome.");
+        } else {
+            System.out.println(num + " is not a palindrome.");
+        }
     }
 }

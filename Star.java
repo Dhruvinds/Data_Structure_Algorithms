e9767f3b172fd8079af78e095ee24236ccb12a60
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-03-27 10:17:15 -0400

    Logic Part with Nested Loop

--- Star.java
+++ Star.java
@@ -1,8 +1,18 @@
 public class Star {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
+        // for (int i = 1; i <= 10; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        char ch = 'A';
+
+        for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }

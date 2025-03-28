4aa9f930a407b17cf454b29ec4933e7a3b67df1c
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-03-28 12:45:45 -0400

    Palidrome Done!

--- Star.java
+++ Star.java
@@ -1,4 +1,18 @@
 public class Star {
+
+    public static void Palidrom(int row, int column) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
+                if (i == 1 || i == row || j == 1 || j == column) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // for (int i = 1; i <= 10; i++) {
         // for (int j = 1; j <= i; j++) {
@@ -7,14 +21,17 @@ public class Star {
         // System.out.println();
         // }
 
-        char ch = 'A';
+        // char ch = 'A';
 
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(ch);
-                ch++;
-            }
-            System.out.println();
-        }
+        // for (int i = 1; i <= 4; i++) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(ch);
+        // ch++;
+        // }
+        // System.out.println();
+        // }
+
+        // Palidrom Pattern
+        Palidrom(4, 5);
     }
 }

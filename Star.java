ae93064d56d880749e9ed458309a87f9921899eb
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-03-29 01:08:01 -0400

    Hollow Rhombus Pattern

--- Star.java
+++ Star.java
@@ -89,16 +89,32 @@ public class Star {
     // }
 
     // public static void SolidRhombus(int n) {
-    //     for (int i = 1; i <= n; i++) {
-    //         for (int j = 1; j <= n - i; j++) {
-    //             System.out.print(" ");
-    //         }
-    //         for (int j = 1; j <= n; j++) {
-    //             System.out.print("*");
-    //         }
-    //         System.out.println();
-    //     }
+    // for (int i = 1; i <= n; i++) {
+    // for (int j = 1; j <= n - i; j++) {
+    // System.out.print(" ");
     // }
+    // for (int j = 1; j <= n; j++) {
+    // System.out.print("*");
+    // }
+    // System.out.println();
+    // }
+    // }
+
+    public static void HollowRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == 1 || i == n || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
         // for (int i = 1; i <= 10; i++) {
@@ -138,5 +154,8 @@ public class Star {
 
         // Solid Rhombus Pattern
         // SolidRhombus(5);
+
+        // Hollow Rhombus
+        HollowRhombus(5);
     }
 }

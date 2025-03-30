91013f2a5a6977bc6cc86522249fd4a6b5f8a3f7
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-03-30 19:21:09 -0400

    Reverse Number

--- Star.java
+++ Star.java
@@ -145,6 +145,15 @@ public class Star {
     // }
     // }
 
+    public static void Reverse_Number(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // for (int i = 1; i <= 10; i++) {
         // for (int j = 1; j <= i; j++) {
@@ -239,22 +248,25 @@ public class Star {
         // int star = 1;
 
         // while (curr_line <= total_line) {
-        //     for (int i = 1; i <= space; i++) {
-        //         System.out.print(" ");
-        //     }
-        //     for (int j = 1; j <= star; j++) {
-        //         System.out.print("*");
-        //     }
-        //     if (curr_line < n) {
-        //         space--;
-        //         star += 2;
-        //     } else {
-        //         space++;
-        //         star -= 2;
-        //     }
-        //     System.out.println();
-        //     curr_line++;
+        // for (int i = 1; i <= space; i++) {
+        // System.out.print(" ");
         // }
+        // for (int j = 1; j <= star; j++) {
+        // System.out.print("*");
+        // }
+        // if (curr_line < n) {
+        // space--;
+        // star += 2;
+        // } else {
+        // space++;
+        // star -= 2;
+        // }
+        // System.out.println();
+        // curr_line++;
+        // }
+
+        // Number increasing Reverse Pyramid
+        Reverse_Number(6);
 
     }
 }

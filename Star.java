227e090f7748b6bff489fefc9a6c0b93b09335a5
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-03-31 00:33:26 -0400

    Number Triangle Pattern

--- Star.java
+++ Star.java
@@ -166,13 +166,25 @@ public class Star {
     // }
     // }
 
+    // public static void NumberTriangle(int n) {
+    // for (int i = 1; i <= n; i++) {
+    // for (int j = 1; j <= i - 1; j++) {
+    // System.out.print(" ");
+    // }
+    // for (int j = i; j <= n; j++) {
+    // System.out.print(j + " ");
+    // }
+    // System.out.println();
+    // }
+    // }
+
     public static void NumberTriangle(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i - 1; j++) {
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = i; j <= n; j++) {
-                System.out.print(j + " ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
@@ -295,6 +307,9 @@ public class Star {
         // Rhombus(5);
 
         // Triangle number
+        // NumberTriangle(6);
+
+        // Number Triangle Pattern
         NumberTriangle(6);
 
     }

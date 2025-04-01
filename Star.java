b8133b4a3afc0e99a69f0b2fa1e7611fb1e72cd8
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-04-01 10:52:55 -0400

    Up to DAte

--- Star.java
+++ Star.java
@@ -217,17 +217,17 @@ public class Star {
     // }
     // }
 
-    public static void InvertedStar(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
+    // public static void InvertedStar(int n) {
+    // for (int i = 1; i <= n; i++) {
+    // for (int j = 1; j < i; j++) {
+    // System.out.print(" ");
+    // }
+    // for (int j = 1; j <= n - i + 1; j++) {
+    // System.out.print("* ");
+    // }
+    // System.out.println();
+    // }
+    // }
 
     public static void main(String[] args) {
         // for (int i = 1; i <= 10; i++) {
@@ -358,7 +358,7 @@ public class Star {
         // TriangleStar(6);
 
         // Inverted Pyramid Star
-        InvertedStar(5);
+        // InvertedStar(5);
 
     }
 }

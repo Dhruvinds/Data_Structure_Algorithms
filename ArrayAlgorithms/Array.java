976851e8bced240cd6ac9c43c1c97b45cd2f6081
Dhruvinds <dhruvinsorathiya6464@gmail.com>
2025-04-01 22:08:40 -0400

    Sub Array Sum Brute Force Approach

---
+++ ArrayAlgorithms/Array.java
@@ -0,0 +1,30 @@
+package ArrayAlgorithms;
+
+import java.util.*;
+
+public class Array {
+    public static void SubArraySum(int number[]) {
+        int curr_sum = 0;
+        int max_sum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i; j < number.length; j++) {
+                curr_sum = 0;
+                for (int k = i; k <= j; k++) {
+                    curr_sum += number[k];
+                }
+                System.out.println(curr_sum);
+                if (max_sum < curr_sum) {
+                    max_sum = curr_sum;
+                }
+            }
+
+        }
+        System.out.println("Max Subarray sum is: " + max_sum);
+    }
+
+    public static void main(String[] args) {
+        int number[] = { 1, -2, 6, -1, 3 };
+        SubArraySum(number);
+    }
+}
